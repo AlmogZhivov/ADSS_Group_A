ec18bdde8d3b170046c1940e9b91bb94387815df
Yali <finkya@post.bgu.ac.il>
2024-05-18 09:37:50 +0300

    Few changes including adding a presentation layer

--- Dev/src/Bussiness/SupplierProduct.java
+++ Dev/src/Bussiness/SupplierProduct.java
@@ -2,18 +2,16 @@ package Bussiness;
 
 public class SupplierProduct {
 
-    private int orderId;
+    private int catalogNumber;
     private String name;
-    private int supplierId;
     private double price;
     private double priceAfterDiscount;
     private int discountPrecentage;
     private int discountThreshold;
 
-    public SupplierProduct(int orderId, String name, int supplierId, double price, double priceAfterDiscount, int discountPrecentage, int discountThreshold) {
-        this.orderId = orderId;
+    public SupplierProduct(int catalogNumber, String name, int supplierId, double price, double priceAfterDiscount, int discountPrecentage, int discountThreshold) {
+        this.catalogNumber = catalogNumber;
         this.name = name;
-        this.supplierId = supplierId;
         this.price = price;
         this.priceAfterDiscount = priceAfterDiscount;
         this.discountPrecentage = discountPrecentage;
---
+++ Dev/src/Presentation/SupplierPresent.java
@@ -0,0 +1,12 @@
+package Presentation;
+
+import java.util.Scanner;
+
+public class SupplierPresent {
+
+    private Scanner scanner;
+
+    public SupplierPresent(){
+        scanner = new Scanner(System.in);
+    }
+}
